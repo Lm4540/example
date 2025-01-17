fce228fab295840b4a9f6483f9270bb26dd20400
unknown <luisrivera4540@gmail.com>
2025-01-17 14:13:58 -0600

    areglado problema al liberar producto con resrva de traslado

--- app/System/Controllers/utilsController.js
+++ app/System/Controllers/utilsController.js
@@ -39,6 +39,127 @@ const UtilsController = {
 
         let invoice = await Sale.findByPk(req.body.sale);
 
+        let details = await sequelize.query(`SELECT * FROM inventory_requisition_detail WHERE requisition in (select id from inventory_requisition where _status = "open") and sale_detail in(SELECT id FROM crm_sale_detail WHERE sale = :sale_id);`, { replacements: { sale_id: invoice.id }, type: QueryTypes.SELECT });
+        if (details.length > 0) {
+            return res.json({
+                status: 'errorMessage', message: "Antes de Anular Libera los detalles solicitados en traslado"
+            });
+        }
+
+        if (invoice) {
+
+            if (invoice._status == 'process' || invoice._status == 'closed' || invoice._status == "prepared") {
+
+                try {
+                    return await sequelize.transaction(async (t) => {
+                        let details = null;
+                        if (invoice.collected > 0.00) {
+                            return res.json({
+                                status: 'errorMessage', message: "No se puede anular esta venta porque tiene pagos asignados, reasigne los pagos para poder Anular esta venta"
+                            });
+                        } else if (invoice.invoice_number !== null) {
+                            return res.json({
+                                status: 'errorMessage', message: "No se puede anular esta venta porque ya ha Facturado"
+                            });
+                        }
+
+                        //buscar el cliente y hacerle una anotacion para que quede costancia
+                        let observation = await ClientObservation.create({
+                            client: invoice.client,
+                            createdBy: req.session.userSession.shortName,
+                            observation: `Se Libero un pedido de $${Helper.money_format(Helper.fix_number(invoice.balance + invoice.delivery_amount))}`,
+                        }, { transaction: t }).catch(e => next(e));
+
+
+                        //Anular la venta
+                        invoice._status = 'revoked';
+                        invoice.revoked_at = new Date();
+                        invoice.revoked_reason = req.body.reason;
+                        invoice.collected = 0.00;
+                        invoice.cost = 0.00;
+                        invoice.payments = null;
+                        invoice.in_report = false;
+                        await invoice.save({ transaction: t });
+
+                        //Liberar los detalles
+                        details = await SaleDetail.findAll({
+                            where: {
+                                sale: invoice.id
+                            }
+                        }, { transaction: t });
+                        let len = details.length;
+                        for (let index = 0; index < len; index++) {
+                            let detail = details[index];
+                            let reserves = await StockReserve.findAll({
+                                where: {
+                                    saleId: detail.id
+                                }
+                            }, { transaction: t });
+
+                            let largo = reserves.length;
+                            for (let a = 0; a < largo; a++) {
+                                let reserve = reserves[a];
+                                let stock = await Stock.findOne({
+                                    where: {
+                                        sucursal: reserve.sucursal,
+                                        product: reserve.product
+                                    }
+                                }, { transaction: t });
+                                if (stock) {
+                                    stock.reserved -= reserve.cant;
+                                    await stock.save({ transaction: t });
+                                }
+
+
+                                let product = await Product.findByPk(reserve.product);
+                                if (product) {
+                                    product.reserved -= reserve.cant;
+                                    await product.save({ transaction: t });
+                                }
+                                await reserve.destroy({ transaction: t });
+                            }
+                            //actualizar el detalle
+
+                            detail.delivered = 0;
+                            detail.reserved = 0;
+                            detail.ready = 0;
+                            await detail.save({ transaction: t });
+                        }
+
+                        return res.json({
+                            status: 'success', invoice: invoice,
+                        });
+
+                    });
+
+                } catch (error) {
+                    return res.json({
+                        status: 'error', message: "Internal Server Error", error: error.message
+                    });
+                }
+            }
+
+            return res.json({
+                status: 'errorMessage', message: "No se puede anular esta venta porque ya ha anulado, ya ha sido entregado el producto o recolectado el pago, por favor anule unicamente la factura"
+            });
+
+
+        }
+        return res.json({
+            status: 'errorMessage', message: "Order not Found"
+        });
+
+    },
+
+    revoke_order_old: async (req, res) => {
+        if (!req.session.userSession.permission.includes('revoke_sales')) {
+            return res.json({
+                status: 'errorMessage', message: "No tienes permiso para realizar esta operación"
+            });
+        }
+
+        let invoice = await Sale.findByPk(req.body.sale);
+
         if (invoice) {
 
             if (invoice._status == 'process' || invoice._status == 'closed' || invoice._status == "prepared") {
